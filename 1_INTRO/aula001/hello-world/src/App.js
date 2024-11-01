61cc6ea8d27795ae02c77c0deeb21456754ae81e
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-11-01 19:44:37 -0300

    +xD

--- 1_INTRO/aula001/hello-world/src/App.js
+++ 1_INTRO/aula001/hello-world/src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World React</h1>
+      <h2>Emmet funcionou</h2>
+      <p>Meu primeiro paragrafo em React</p>
     </div>
   );
 }
