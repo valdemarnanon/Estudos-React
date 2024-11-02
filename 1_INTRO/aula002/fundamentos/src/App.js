2751086dd63421c3e84c7c329f4d5ed876b1bfad
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-11-01 21:13:21 -0300

    ++

---
+++ 1_INTRO/aula002/fundamentos/src/App.js
@@ -0,0 +1,17 @@
+// Components
+import FistComponent from './components/FistComponents';
+
+
+// styles / CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FistComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ 1_INTRO/aula002/fundamentos/src/components/FistComponents.jsx
@@ -0,0 +1,15 @@
+// Arquivo de estilo
+
+function FistComponent() {
+    // essa função faz isso
+
+    return (
+        <div>
+        {/*Algum comentario*/}
+            <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
+        </div>
+    );
+}
+
+export default FistComponent;
