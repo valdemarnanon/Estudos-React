55021d41320d009ca89a391738c96f66335cb730
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-11-03 16:57:04 -0300

    React

--- 1_INTRO/aula002/fundamentos/src/components/Challenge.jsx
+++ 1_INTRO/aula002/fundamentos/src/components/Challenge.jsx
@@ -3,15 +3,11 @@ export default function Challenge() {
     const n1 = 5;
     const n2 = 30;
 
-    const soma = () => {
-        return console.log(n1 + n2)
-    }
-
     return (
         <div>
             <h1>Numero um: {n1}</h1>
             <h1>Numero dois: {n2}</h1>
-            <button onClick={soma}>Soma numeros</button>
+            <button onClick={() => console.log(n1 + n2)}>Soma numeros</button>
         </div>
     )
 }
