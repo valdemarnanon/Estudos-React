0a9913d9acada102ca11afa17a19ab414cc08ee3
valdemarnanon <139665106+valdemarnanon@users.noreply.github.com>
2024-11-13 16:38:33 -0300

    Formularios em React

--- 5_FORM_REACT/form/src/App.js
+++ 5_FORM_REACT/form/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyForm from "./components/MyForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Formularios</h1>
+      <MyForm
+        user={{
+          name: "Valdemar",
+          email: "valdemarnanon@gmail.com",
+          bio: "Sou um programador",
+          role: "admin",
+        }}
+      />
     </div>
   );
 }
-
 export default App;
